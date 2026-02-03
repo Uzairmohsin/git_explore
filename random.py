a385a2476b0b491d6acccf0c3d7a3170ba7044de
Uzairmohsin <uzairmohsinsk@gmail.com>
2026-02-03 21:10:36 +0530

    comment

--- random.py
+++ random.py
@@ -663,14 +663,14 @@ print(hi.format( name, country))
   
 # clutter()
 
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
     
-    @classmethod
-    def form_string(cls, string):
-        name, age = string.split(',')
-        return cls(name,int(age))
-person = Person.form_string("John Doe, 30")
-print(person.name,person.age)
+#     @classmethod
+#     def form_string(cls, string):
+#         name, age = string.split(',')
+#         return cls(name,int(age))
+# person = Person.form_string("John Doe, 30")
+# print(person.name,person.age)
