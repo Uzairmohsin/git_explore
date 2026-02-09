b7a87f27f4cca2ad9bc0fcc17d4df36ccb2b33ca
Uzairmohsin <uzairmohsinsk@gmail.com>
2026-02-09 23:54:03 +0530

    Other way of pdf merge

--- pdf_project/pdfs/merge_pdf.py
+++ pdf_project/pdfs/merge_pdf.py
@@ -1,28 +1,42 @@
+# import os
+# from pypdf import PdfReader, PdfWriter
+
+# def merge_pdfs():
+#     writer = PdfWriter()
+
+#     script_dir = os.path.dirname(os.path.abspath(__file__))
+
+#     for file in os.listdir(script_dir):
+#         if file.endswith(".pdf"):
+#             file_path = os.path.join(script_dir, file)
+#             print("Merging:", file)
+
+#             reader = PdfReader(file_path)
+#             for page in reader.pages:
+#                 writer.add_page(page)
+
+#     if len(writer.pages) == 0:
+#         print("No pages were added. Check input PDFs.")
+#         return
+
+#     output_path = os.path.join(script_dir, "merged_output.pdf")
+#     with open(output_path, "wb") as f:
+#         writer.write(f)
+
+#     print("PDFs merged successfully!")
+
+# merge_pdfs()
+
+# Other way
+
+from PyPDF2 import PdfWriter
 import os
-from pypdf import PdfReader, PdfWriter
 
-def merge_pdfs():
-    writer = PdfWriter()
+merger = PdfWriter()
+files = [file for file in os.listdir() if file.endswith(".pdf")]
 
-    script_dir = os.path.dirname(os.path.abspath(__file__))
+for pdf in files:
+    merger.append(pdf)
 
-    for file in os.listdir(script_dir):
-        if file.endswith(".pdf"):
-            file_path = os.path.join(script_dir, file)
-            print("Merging:", file)
-
-            reader = PdfReader(file_path)
-            for page in reader.pages:
-                writer.add_page(page)
-
-    if len(writer.pages) == 0:
-        print("No pages were added. Check input PDFs.")
-        return
-
-    output_path = os.path.join(script_dir, "merged_output.pdf")
-    with open(output_path, "wb") as f:
-        writer.write(f)
-
-    print("PDFs merged successfully!")
-
-merge_pdfs()
+merger.write("merged-pdf.pdf")
+merger.close()
